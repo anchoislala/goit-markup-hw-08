66d002ffcb9492493479a3b25d96fa771cf15d11
Anna Leszka <ania.lalala97@gmail.com>
2021-12-24 13:41:15 +0100

    js

---
+++ js/mobile-menu.js
@@ -0,0 +1,53 @@
+// (() => {
+//     const menuBtnRef = document.querySelector(".mobile-button-open");
+//     const mobileMenuRef = document.querySelector("[data-menu]");
+//     const menuBtnClose = document.querySelector(".mobile-button-close");
+
+//         menuBtnRef.addEventListener("click", () => {
+//             menuBtnRef.classList.toggle("is-open");
+//             mobileMenuRef.classList.toggle("is-open");
+            
+//         });
+//     menuBtnClose.addEventListener("click", () => {
+//         menuBtnRef.classList.toggle("is-open");
+//         mobileMenuRef.classList.toggle("is-open");
+//     });
+
+//       window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+//     if (!e.matches) return;
+//     mobileMenu.classList.remove('is-open');
+//     openMenuBtn.setAttribute('aria-expanded', false);
+//     bodyScrollLock.enableBodyScroll(document.body);
+//   });
+
+// })();
+
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
